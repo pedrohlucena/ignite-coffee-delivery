6ba0464eb9c1c2678e8c6b50e7871f220c107806
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-07-30 08:48:08 -0300

    create default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/themes/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
