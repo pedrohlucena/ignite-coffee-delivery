2d934f1ef826273c0b1693dcc0a4d7f18a2eacf5
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-07-30 08:17:02 -0300

    create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { Button } from './components/Button'
+import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Button />
     </ThemeProvider>
   )
--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -2,22 +2,22 @@ import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     ${({ theme }) => css`
-      /* * {
+      * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-      } */
+      }
 
-      /* :focus {
+      :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${theme['green-500']};
-      } */
+        box-shadow: 0 0 0 2px ${theme.yellow};
+      }
 
-      /* body {
-        background: ${theme['gray-900']};
-        color: ${theme['gray-300']};
+      body {
+        background: ${theme.background};
+        color: ${theme['base-text']};
         -webkit-font-smoothing: antialiased;
-      } */
+      }
 
       body,
       input,
