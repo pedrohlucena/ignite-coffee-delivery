9f0ee9ae71072f42c7f883358fe903b26eac47c9
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-08-01 07:01:46 -0300

    create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,28 @@
+import { NavLink } from 'react-router-dom'
+import coffeeDeliveryLogo from '../../assets/coffee-delivery-logo.svg'
+import shoopingCart from '../../assets/shopping-cart.svg'
+import { MapPin } from '@phosphor-icons/react'
 import * as S from './styles'
 
 export function Header() {
-  return <S.HeaderContainer>Header</S.HeaderContainer>
+  return (
+    <S.HeaderContainer>
+      <NavLink to="/" title="Home">
+        <img src={coffeeDeliveryLogo} alt="Coffee Delivery" />
+      </NavLink>
+
+      <S.Actions>
+        <S.LocationButton>
+          <MapPin size={22} weight="fill" />
+          São Paulo, SP
+        </S.LocationButton>
+
+        <NavLink to="/checkout" title="Carrinho">
+          <S.CartButton>
+            <img src={shoopingCart} alt="Carrinho de compras" />
+          </S.CartButton>
+        </NavLink>
+      </S.Actions>
+    </S.HeaderContainer>
+  )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,81 @@
-import { styled } from 'styled-components'
+import { css, styled } from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem 10rem;
+`
+
+export const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+export const LocationButton = styled.button`
+  ${({ theme }) => css`
+    border: 0;
+    padding: 0.5rem;
+    border-radius: 6px;
+
+    display: flex;
+    align-items: center;
+    background: ${theme['purple-light']};
+    color: ${theme['purple-dark']};
+
+    font-size: 0.875rem;
+
+    gap: 0.25rem;
+
+    cursor: pointer;
+
+    > svg {
+      fill: ${theme.purple};
+    }
+  `}
+`
+
+export const CartButton = styled.div`
+  ${({ theme }) => {
+    return css`
+      display: flex;
+      padding: 0.5rem;
+
+      border-radius: 0.375rem;
+      background: ${theme['yellow-light']};
+
+      cursor: pointer;
+
+      > img {
+        width: 1.375rem;
+        height: 1.375rem;
+      }
+
+      &:after {
+        content: '3';
+
+        display: flex;
+        width: 1.25rem;
+        height: 1.25rem;
+        justify-content: center;
+        align-items: center;
+
+        position: absolute;
+        margin-left: 1rem;
+        margin-top: -1rem;
+
+        border-radius: 1000px;
+        background: ${theme['yellow-dark']};
+
+        flex-shrink: 0;
+
+        color: ${theme.white};
+        text-align: center;
+
+        font-size: 0.75rem;
+        font-weight: 700;
+      }
+    `
+  }}
+`
